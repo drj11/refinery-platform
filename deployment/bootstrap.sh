d4a622d3db61e6d6ce5ccbd4bd3251c2b2fc5190
David Jones <drj@pobox.com>
2015-11-18 14:07:49 +0000

    git clone from public github repo

--- deployment/bootstrap.sh
+++ deployment/bootstrap.sh
@@ -5,4 +5,5 @@ export DEBIAN_FRONTEND=noninteractive
 /usr/bin/apt-get -qq update && /usr/bin/apt-get -q -y install make puppet git ruby-dev
 
 /usr/bin/gem install librarian-puppet -v 2.2.1 --no-rdoc --no-ri
-cd /vagrant/deployment && /usr/local/bin/librarian-puppet install
+git clone https://github.com/parklab/refinery-platform.git /srv/refinery-platform
+cd /srv/refinery-platform/deployment && /usr/local/bin/librarian-puppet install
