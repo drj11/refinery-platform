dfd3978c89de2250846246a46f5dafb9d3c5dae1
David Jones <drj@pobox.com>
2015-11-19 14:09:41 +0000

    Add htop

--- deployment/bootstrap.sh
+++ deployment/bootstrap.sh
@@ -2,7 +2,10 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-/usr/bin/apt-get -qq update && /usr/bin/apt-get -q -y install make puppet git ruby-dev
+/usr/bin/apt-get -qq update
+OPERATIONS="htop"
+DEVELOPMENT="make puppet git ruby-dev"
+/usr/bin/apt-get -q -y install $OPERATIONS $DEVELOPMENT
 
 /usr/bin/gem install librarian-puppet -v 2.2.1 --no-rdoc --no-ri
 mkdir /srv/refinery-platform
