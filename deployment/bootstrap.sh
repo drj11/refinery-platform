97fecc229a5d33c51368819982c88f17ce84945b
David Jones <drj@pobox.com>
2015-11-20 13:55:44 +0000

    Remove AWS-specific code from bootstrap.sh

--- deployment/bootstrap.sh
+++ deployment/bootstrap.sh
@@ -8,7 +8,3 @@ DEVELOPMENT="make puppet git ruby-dev"
 /usr/bin/apt-get -q -y install $OPERATIONS $DEVELOPMENT
 
 /usr/bin/gem install librarian-puppet -v 2.2.1 --no-rdoc --no-ri
-mkdir /srv/refinery-platform
-chown ubuntu:ubuntu /srv/refinery-platform
-sudo su -c 'git clone https://github.com/parklab/refinery-platform.git /srv/refinery-platform' ubuntu
-cd /srv/refinery-platform/deployment && /usr/local/bin/librarian-puppet install
