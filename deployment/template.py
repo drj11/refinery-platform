e81b16239b3ef7849d5c7912b626d7582878165d
David Jones <drj@pobox.com>
2015-11-30 15:32:44 +0000

    Change the logical id of the instance

--- deployment/template.py
+++ deployment/template.py
@@ -9,7 +9,7 @@ cft = CloudFormationTemplate(description="Single instance template.")
 user_data_script = open('bootstrap.sh').read() + open('aws.sh').read()
 
 cft.resources.ec2_instance = Resource(
-    'AnInstance', 'AWS::EC2::Instance',
+    'MonolithicInstance', 'AWS::EC2::Instance',
     Properties({
         'ImageId': 'ami-d05e75b8',
         'InstanceType': 'm3.medium',
